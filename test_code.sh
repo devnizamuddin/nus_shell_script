7846a53705d4ef451dc063a5f06c4de46c3d69ed
devnizamuddin <dev.nizamuddin@gmail.com>
2025-08-18 00:43:10 +0600

    code updated

--- test_code.sh
+++ test_code.sh
@@ -6,7 +6,7 @@
 #* ╚══════════════════════════════════════════════════════════════════════════════════════════════════╝
 #*
 
-source ./text_style.sh
+. ./text_style.sh
 # source ./printer.sh
 
 
@@ -44,15 +44,53 @@ BG_BLUE='\033[44m'
 BG_MAGENTA='\033[45m'
 BG_CYAN='\033[46m'
 BG_WHITE='\033[47m'
-TEXT_COLOR='\033[1;37m'
 BG_GOLDEN='\033[48;5;214m'  # Extended color for golden background
 BG_GREEN='\033[42m'
 
+BG_BLUE_GREY="\033[48;5;67m" 
 
 #* Reset the style
 RESET='\033[0m'
 NC='\033[0m'
 
 
-echo "Hello World"
+echo -e "${BG_BLUE_GREY}${WHITE} Hello World${RESET}"
 
+#!/bin/sh
+
+RESET="\033[0m"
+BOLD="\033[1m"
+FG_WHITE="\033[97m"
+BG_BLUEGREY="\033[48;2;96;125;139m"   # Blue Grey (RGB)
+
+printHeader() {
+    title="$1"
+    width=80
+
+    # count characters (not bytes)
+    title_length=$(printf "%s" "$title" | wc -m)
+
+    # ⚠️ adjustment: some emoji take 2 cells, so rough fix
+    # যদি title এ emoji থাকে, ধরে নিলাম প্রতিটি emoji 2 width
+    emoji_count=$(printf "%s" "$title" | grep -o "🐙" | wc -l)
+    title_length=$(( title_length + emoji_count ))
+
+    padding=$(( (width - title_length) / 2 ))
+    [ $padding -lt 0 ] && padding=0
+
+    # top border
+    printf "${BG_BLUEGREY}%${width}s${RESET}\n" "" | tr ' ' '='
+
+    # centered title
+    printf "${BG_BLUEGREY}${BOLD}${FG_WHITE}%*s%s%*s${RESET}\n" \
+        "$padding" "" "$title" "$padding" ""
+
+    # bottom border
+    printf "${BG_BLUEGREY}%${width}s${RESET}\n" "" | tr ' ' '='
+}
+
+# Example
+printHeader "🐙 Welcome to My Script"
+
+
+printHeader "🐙 Welcome to My Script    "
