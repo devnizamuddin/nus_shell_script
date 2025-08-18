b938799e79e35f99b76bf6623aba64c9b52fb366
devnizamuddin <dev.nizamuddin@gmail.com>
2025-08-18 18:22:20 +0600

    ongoing

--- text_style.sh
+++ text_style.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# text_style.sh
 
 #*
 #* ┏==================================================================================================┓
@@ -7,40 +6,40 @@
 #* ┗==================================================================================================┛
 #*
 
-#* Text Color
+#*
+#* ---> Foreground Color
 #
-RED='\033[31m'
-GREEN='\033[32m'
-BLUE='\033[34m'
-YELLOW='\033[33m'
-MAGENTA='\033[35m'
-CYAN='\033[36m'
-WHITE='\033[97m'
-GOLDEN='\033[38;5;214m'
+export RED='\033[31m'
+export GREEN='\033[32m'
+export BLUE='\033[34m'
+export YELLOW='\033[33m'
+export MAGENTA='\033[35m'
+export CYAN='\033[36m'
+export WHITE='\033[97m'
+export GOLDEN='\033[38;5;214m'
 #
-#* Text Style
+#* ---> Text Style
 #
-BOLD='\033[1m'
-DIM='\033[2m'
-UNDERLINE='\033[4m'
-BLINK='\033[5m'
-REVERSE='\033[7m'
-STRIKETHROUGH='\033[9m'
+export BOLD='\033[1m'
+export DIM='\033[2m'
+export UNDERLINE='\033[4m'
+export BLINK='\033[5m'
+export REVERSE='\033[7m'
+export STRIKETHROUGH='\033[9m'
 #
-#* Background Color Variables
+#* ---> Background Color 
 #
-BG_RED='\033[41m'
-BG_YELLOW='\033[43m'
-BG_BLUE='\033[44m'
-BG_MAGENTA='\033[45m'
-BG_CYAN='\033[46m'
-BG_WHITE='\033[47m'
-TEXT_COLOR='\033[1;37m'
-BG_GOLDEN='\033[48;5;214m'  # Extended color for golden background
-BG_GREEN='\033[42m'
+export BG_RED='\033[41m'
+export BG_YELLOW='\033[43m'
+export BG_BLUE='\033[44m'
+export BG_MAGENTA='\033[45m'
+export BG_CYAN='\033[46m'
+export BG_WHITE='\033[47m'
+export TEXT_COLOR='\033[1;37m'
+export BG_GOLDEN='\033[48;5;214m'  # Extended color for golden background
+export BG_GREEN='\033[42m'
 
 
 #* Reset the style
-RESET='\033[0m'
-NC='\033[0m'
+export RESET='\033[0m'
 
